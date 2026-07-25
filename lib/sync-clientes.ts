6c3806fd43819d006ce0fc7c61627bc71f428de7
CesarAugustoPrado1 <128118934+CesarAugustoPrado1@users.noreply.github.com>
2026-07-25 11:59:14 -0300

    Add new client attributes and update sync logic

--- lib/sync-clientes.ts
+++ lib/sync-clientes.ts
@@ -5,14 +5,16 @@ const SOAP_URL = 'http://wspirkastone.pypcloud.net:1881/ServicioCCOCliente.asmx'
 const atributos = [
   'ClienteID', 'Nombre', 'NombreLegal', 'Domicilio', 'Localidad',
   'CodigoPostal', 'Provincia', 'Pais', 'Telefono', 'Fax', 'Email',
-  'Observacion', 'CondicionAnteElIVA', 'IngresosBrutos',
+  'Observacion', 'CondicionAnteElIVA', 'ClaveTributaria', 'IngresosBrutos',
   'ContactoDeVenta', 'ContactoDeCobros', 'CondicionPago',
   'MonedaUsualCuentaCorriente', 'CuentaCliente', 'TipoDeCliente',
-  'ActividadDeCliente', 'Vendedor', 'ZonaDeVenta',
-  'Cobrador', 'BloqueadoParaNotasDePedido',
+  'ActividadDeCliente', 'Clasificacion1', 'Clasificacion2', 'Clasificacion3',
+  'Clasificacion4', 'Clasificacion5', 'Clasificacion6', 'Clasificacion7',
+  'Clasificacion8', 'Clasificacion9', 'Vendedor', 'ZonaDeVenta',
+  'Cobrador', 'Transporte', 'BloqueadoParaNotasDePedido',
   'BloqueadoParaFacturar', 'FechaDeAlta', 'FechaDeBaja',
-  'HabilitadoParaConsultasWeb', 'FechaUltActualizacion'
-  // Eliminamos Clasificacion1-9, ClaveTributaria, Transporte, FormatoDeImpresion, Vendedor2
+  'HabilitadoParaConsultasWeb', 'FormatoDeImpresionPorCliente',
+  'Vendedor2', 'FechaUltActualizacion'
 ];
 
 export async function syncClientes() {
@@ -28,7 +30,7 @@ export async function syncClientes() {
     idAttr: 'ClienteID',
     tabla: 'clientes',
     idCol: 'clienteid',
-    limite: 100, // Para pruebas rápidas
+    limite: 0, // 0 = sin límite (sincroniza todos)
     mapear: (item: any) => {
       const getValor = (node: any, campo: string): string | null => {
         if (node.$ && node.$[campo] !== undefined) return node.$[campo];
@@ -44,38 +46,41 @@ export async function syncClientes() {
         localidad: getValor(item, 'Localidad'),
         codigopostal: getValor(item, 'CodigoPostal'),
         provincia: getValor(item, 'Provincia'),
-        provincianombre: getValor(item, 'ProvinciaNombre'),
         pais: getValor(item, 'Pais'),
-        paisnombre: getValor(item, 'PaisNombre'),
         telefono: getValor(item, 'Telefono'),
         fax: getValor(item, 'Fax'),
         email: getValor(item, 'Email'),
         observacion: getValor(item, 'Observacion'),
         condicionanteeliva: getValor(item, 'CondicionAnteElIVA'),
-        condicionanteelivanombre: getValor(item, 'CondicionAnteElIVANombre'),
+        clavetributaria: getValor(item, 'ClaveTributaria'),
         ingresosbrutos: getValor(item, 'IngresosBrutos'),
         contactodeventa: getValor(item, 'ContactoDeVenta'),
         contactodecobros: getValor(item, 'ContactoDeCobros'),
         condicionpago: getValor(item, 'CondicionPago'),
-        condicionpagonombre: getValor(item, 'CondicionPagoNombre'),
         monedausualcuentacorriente: getValor(item, 'MonedaUsualCuentaCorriente'),
-        monedausualcuentacorrientenombre: getValor(item, 'MonedaUsualCuentaCorrienteNombre'),
         cuentacliente: getValor(item, 'CuentaCliente'),
         tipodecliente: getValor(item, 'TipoDeCliente'),
-        tipodeclientenombre: getValor(item, 'TipoDeClienteNombre'),
         actividaddecliente: getValor(item, 'ActividadDeCliente'),
-        actividaddeclientenombre: getValor(item, 'ActividadDeClienteNombre'),
+        clasificacion1: getValor(item, 'Clasificacion1'),
+        clasificacion2: getValor(item, 'Clasificacion2'),
+        clasificacion3: getValor(item, 'Clasificacion3'),
+        clasificacion4: getValor(item, 'Clasificacion4'),
+        clasificacion5: getValor(item, 'Clasificacion5'),
+        clasificacion6: getValor(item, 'Clasificacion6'),
+        clasificacion7: getValor(item, 'Clasificacion7'),
+        clasificacion8: getValor(item, 'Clasificacion8'),
+        clasificacion9: getValor(item, 'Clasificacion9'),
         vendedor: getValor(item, 'Vendedor'),
-        vendedornombre: getValor(item, 'VendedorNombre'),
         zonadeventa: getValor(item, 'ZonaDeVenta'),
-        zonadeventanombre: getValor(item, 'ZonaDeVentaNombre'),
         cobrador: getValor(item, 'Cobrador'),
-        cobradornombre: getValor(item, 'CobradorNombre'),
+        transporte: getValor(item, 'Transporte'),
         bloqueadoparanotasdepedido: parseBooleano(getValor(item, 'BloqueadoParaNotasDePedido')),
         bloqueadoparafacturar: parseBooleano(getValor(item, 'BloqueadoParaFacturar')),
         fechadealta: parseFecha(getValor(item, 'FechaDeAlta')),
         fechadebaja: parseFecha(getValor(item, 'FechaDeBaja')),
         habilitadoparaconsultasweb: parseBooleano(getValor(item, 'HabilitadoParaConsultasWeb')),
+        formatodeimpresionporcliente: getValor(item, 'FormatoDeImpresionPorCliente'),
+        vendedor2: getValor(item, 'Vendedor2'),
         fechaultactualizacion: parseFecha(getValor(item, 'FechaUltActualizacion')),
       };
     }
