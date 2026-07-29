138e2f309be6ad3c2b5d6266850f3d6695beee67
CesarAugustoPrado1 <128118934+CesarAugustoPrado1@users.noreply.github.com>
2026-07-29 15:18:08 -0300

    Update sync-all.ts

--- scripts/sync-all.ts
+++ scripts/sync-all.ts
@@ -1,7 +1,7 @@
 import { syncProductos } from '../lib/sync-productos';
 import { syncClientes } from '../lib/sync-clientes';
 import { syncNotasDePedido } from '../lib/sync-notas-pedido';
-import { syncNotasDePedidoDetalle } from '../lib/sync-notas-pedido-detalle';
+import { syncNotasPedidoDetalle } from '../lib/sync-notas-pedido-detalle';
 
 export async function syncAll() {
   await syncProductos();
